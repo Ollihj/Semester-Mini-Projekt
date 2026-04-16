d7d7f654f536cea43c3d2563b39e3e672ea24185
berthelsenmarius-debug <berthelsenmarius@gmail.com>
2026-04-16 13:29:10 +0200

    gjort så man ikke kan se annoncerne hvis man ikke er logget ind og nu kan man logge ud

--- BlazorApp4/Program.cs
+++ BlazorApp4/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp4;
 using BlazorApp4.Pages;
 using BlazorApp4.Service;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,5 +13,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(Config.ServerUrl) });
 builder.Services.AddScoped<IItemService, ItemServiceHttp>();
 builder.Services.AddScoped<LoginService>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
